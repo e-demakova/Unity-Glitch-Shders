3b5883c26188ecef01da1f27ebe024e292217ec6
Artem Matiashov <demakovaln@mail.ru>
2021-10-11 16:59:41 +0700

    Values are saved to SO

---
+++ Assets/Effects/EffectsValues.cs
@@ -0,0 +1,23 @@
+﻿using Effects.ColorAberration;
+using Effects.Digital;
+using UnityEngine;
+
+namespace Effects
+{
+    [CreateAssetMenu(fileName = "NewEffectsValues", menuName = "Effects Values")]
+    public class EffectsValues : ScriptableObject
+    {
+        public ColorGlitch.Values Color;
+        public DigitalGlitch.Values DigitalA;
+        public DigitalGlitch.Values DigitalB;
+        public float Duration;
+
+        public void Init(ColorGlitch color, DigitalGlitch digitalA, DigitalGlitch digitalB, float duration)
+        {
+            Color = color.Settings;
+            DigitalA = digitalA.Settings;
+            DigitalB = digitalB.Settings;
+            Duration = duration;
+        }
+    }
+}
